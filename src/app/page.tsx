43118ec9a3b0dca36bfa859979a3e33c7fd0afd1
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-08-24 17:32:15 -0400

    feat: Habilita restricción para todas las páginas, moviendo todo a la página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Task from "@/app/components/task";
 
-export default function Home() {
+export default function Todo() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Welcome to the Todo App</h1>
-      <div className="flex justify-end mt-4">
-        <Button asChild>
-          <Link href="/todo">Todo</Link>
-        </Button>
-      </div>
-    </div>
+
+    <>
+      <Task />
+    </>
   );
 }
